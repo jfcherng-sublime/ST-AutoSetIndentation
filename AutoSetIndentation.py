cd88e4d8f6fe6e42b78a02da96bd3e423bb5d18f
Jack Cherng <jfcherng@gmail.com>
2019-02-24 18:23:42 +0800

    Code tidy
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- AutoSetIndentation.py
+++ AutoSetIndentation.py
@@ -23,27 +23,29 @@ def plugin_loaded():
     settings = sublime.load_settings(PLUGIN_SETTINGS)
 
     # when the user settings is modified
-    settings.add_on_change(PLUGIN_SETTINGS, pluginSettingsListener)
+    settings.add_on_change(PLUGIN_SETTINGS, plugin_settings_listener)
 
 
-def pluginSettingsListener():
+def plugin_settings_listener():
     """ called when plugin's settings file is changed """
 
     global settings
 
 
-def printPluginMessage(message):
-    print(pluginMessage(message))
+def print_plugin_message(message):
+    print(plugin_message(message))
 
 
-def pluginMessage(message):
+def plugin_message(message):
     return '[{0}] {1}'.format(PLUGIN_NAME, message)
 
 
 class AutoSetIndentationCommand(sublime_plugin.TextCommand):
-    def run(self, edit, show_message=True):
+    """Examines the contents of the buffer to determine the indentation settings."""
+
+    def run(self, edit, show_message=True, threshold=10):
         """
-        @brief Examines the contents of the buffer to determine the indentation settings.
+        @brief Run the command.
 
         @param self         The object
         @param edit         The edit
@@ -51,9 +53,26 @@ class AutoSetIndentationCommand(sublime_plugin.TextCommand):
         """
 
         sample = self.view.substr(sublime.Region(0, min(self.view.size(), 2**14)))
+        indent_tab, indent_space = self.guess_indentation_from_string(sample)
 
+        # more like mixed-indented
+        if indent_tab > 0 and indent_space > 0:
+            self.set_mixed_indentation(indent_tab, indent_space)
+            return
+
+        # tab-indented
+        if indent_tab > 0:
+            self.set_tab_indentation(indent_tab)
+            return
+
+        # space-indented
+        if indent_space > 0:
+            self.set_space_indentation(indent_space)
+            return
+
+    def guess_indentation_from_string(self, string):
         indent_finder = IndentFinder()
-        indent_finder.parse_string(sample)
+        indent_finder.parse_string(string)
 
         # possible outputs:
         #
@@ -70,20 +89,30 @@ class AutoSetIndentationCommand(sublime_plugin.TextCommand):
         indent_space = re.search(r'\bspace\s+([0-9]+)', result)
         indent_space = int(indent_space.group(1)) if indent_space else 0
 
-        if indent_tab > indent_space:
-            self.view.settings().set('translate_tabs_to_spaces', False)
-            self.view.settings().set('tab_size', indent_tab)
-            self.show_status_message(
-                pluginMessage('Indentation: tabs'),
-                show_message
-            )
-        else:
-            self.view.settings().set('translate_tabs_to_spaces', True)
-            self.view.settings().set('tab_size', indent_space)
-            self.show_status_message(
-                pluginMessage('Indentation: %d spaces' % indent_space),
-                show_message
-            )
+        return (indent_tab, indent_space)
+
+    def set_mixed_indentation(self, indent_tab=4, indent_space=4, show_message=True):
+        self.set_tab_indentation(indent_tab, False)
+        self.show_status_message(
+            plugin_message('Indentation: tab/%d space/%d (mixed)' % (indent_tab, indent_space)),
+            show_message
+        )
+
+    def set_tab_indentation(self, indent_tab=4, show_message=True):
+        self.view.settings().set('translate_tabs_to_spaces', False)
+        self.view.settings().set('tab_size', indent_tab)
+        self.show_status_message(
+            plugin_message('Indentation: tab/%d' % indent_tab),
+            show_message
+        )
+
+    def set_space_indentation(self, indent_space=4, show_message=True):
+        self.view.settings().set('translate_tabs_to_spaces', True)
+        self.view.settings().set('tab_size', indent_space)
+        self.show_status_message(
+            plugin_message('Indentation: space/%d' % indent_space),
+            show_message
+        )
 
     def show_status_message(self, message, show_message=True):
         if show_message:
@@ -159,7 +188,7 @@ class AutoSetIndentationEventListener(sublime_plugin.EventListener):
         try:
             return settings.get('event_listeners', None)[event]
         except:
-            printPluginMessage(
+            print_plugin_message(
                 '"%s" is not set in user settings (assumed false)' %
                 ('event_listeners.' + event)
             )
